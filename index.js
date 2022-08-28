5e3fab44a23ec03f0905afae7457184f8e9149dc
Elena <106665871+elenadanchenkomn@users.noreply.github.com>
2022-08-28 19:15:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,156 @@
+const cardOne = document.querySelector('.oneCard');
+const cardTwo = document.querySelector('.twoCard');
+const cardThree = document.querySelector('.threeCard');
+const carFor = document.querySelector('.forCard');
+const mixCards = document.querySelector('.mixTheCards');
+const butBut = document.querySelector('.but');
+const topButton = document.querySelector('.top');
+const allCards = document.querySelector('.cards');
+const allButtons = document.querySelector('.buttons');
+const cardDesk = document.querySelector('.card-desk');
+const tableStage = document.querySelector('.table');
+const cardsArray = document.querySelectorAll('.all');
+const divCard = document.querySelector('.pictures');
+const imgCard = document.querySelector('.picture');
+
+let selectedTd;
+
+allCards.onclick = function(event) {
+  let target = event.target;
+
+  if (target.tagName != 'IMG') return; 
+
+  highlight(target); 
+};
+
+function highlight(img) {
+  if (selectedTd) { 
+    selectedTd.classList.remove('red');
+  }
+  selectedTd = img;
+  selectedTd.classList.add('red');  
+};
+
+//buttons
+let select;
+allButtons.onclick = function(event) {
+    let target = event.target; 
+  
+    if (target.tagName != 'BUTTON') return; 
+    light(target); 
+    mixCards.classList.add('notNone')
+  };
+  
+  function light(button) {
+    if (select) { 
+      select.classList.remove('red1');
+    }
+    select = button;
+    select.classList.add('red1'); 
+  };
+//mixCard-display none
+
+mixCards.addEventListener('click', function(){
+   
+    mixCards.classList.remove('notNone')
+    cardDesk.classList.add('card-desk-notNone')
+    tableStage.classList.add('table_block')
+});
+
+//mix desk 
+
+
+
+  
+ 
+
+const cardsData = [
+    {  src: '="https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue1.png?raw=true" ' ,
+      id: 'blue1',
+      
+      difficulty: 'hard',
+      color:'blue'
+    },
+    { src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue2.png?raw=true' ,
+      id: 'blue2',
+      
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {src: 'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue3.png?raw=true',
+      id: 'blue3',
+     
+      difficulty: 'easy',
+      color:'blue'
+    },
+   
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue4.png?raw=true' ,
+      id: 'blue4',
+      
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue5.png?raw=true' ,
+      id: 'blue5',
+      
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue6.png?raw=true' ,
+      id: 'blue6',
+      
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue7.png?raw=true' ,
+      id: 'blue7',
+      
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue8.png?raw=true' ,
+      id: 'blue8',
+     
+      difficulty: 'hard',
+      color:'blue'
+    },
+    {src: 'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue9.png?raw=true',
+      id: 'blue9',
+     
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {src: 'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue10.png?raw=true',
+      id: 'blue10',
+      
+      difficulty: 'easy',
+      color:'blue'
+    },
+    {src:'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue12.png?raw=true' ,
+      id: 'blue11',
+     
+      difficulty: 'normal',
+      color:'blue'
+    },
+    {src: 'https://github.com/elenadanchenkomn/eldritch-codejam/blob/main/assets/MythicCards/blue/blue11.png?raw=true',
+      id: 'blue12',
+      
+      difficulty: 'normal',
+      color:'blue'
+    },
+  ]
+  let srccard;
+  cardsData.forEach(i=>{
+srccard = i.src ;
+  
+})
+  
+  cardDesk.addEventListener('click',function(srccard){ 
+ 
+imgCard.setAttribute('src' ,srccard);
+
+});
+ 
+
+   
+         
